cf61f266dcc03ae99044033a505c757b6b64ef72
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2023-08-23 15:40:06 +0600

    creating userList page

--- src/Components/Admin/UserList.jsx
+++ src/Components/Admin/UserList.jsx
@@ -1,7 +1,166 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 export default function UserList() {
+  const [userList, setUserList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  useEffect(() => {
+    fetch("http://192.168.1.29:8081/admin/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUserList(data);
+      });
+  }, [userList]);
+
+  const totalPages = Math.ceil(userList.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = userList.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (pageNumber) => {
+    if (pageNumber >= 1 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+    }
+  };
+
+  const handleGoButtonClick = () => {
+    const inputPage = parseInt(document.getElementById("jumpPageInput").value);
+    if (!isNaN(inputPage)) {
+      handlePageChange(inputPage);
+    }
+  };
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+
+    const maxButtons = 4;
+    let startPage = Math.max(1, currentPage - Math.floor(maxButtons / 2));
+    let endPage = Math.min(totalPages, startPage + maxButtons - 1);
+
+    if (endPage - startPage + 1 < maxButtons) {
+      startPage = Math.max(1, endPage - maxButtons + 1);
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(
+        <button
+          key={i}
+          className={`btn btn-sm bg-blue-200 mr-2 ${
+            i === currentPage ? "bg-blue-500" : ""
+          }`}
+          onClick={() => handlePageChange(i)}
+        >
+          {i}
+        </button>
+      );
+    }
+    return pageNumbers;
+  };
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const url = `http://192.168.1.29:8081/admin/users/${id}`;
+
+        fetch(url, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            setUserList((prevUsers) =>
+              prevUsers.filter((user) => user.id !== id)
+            );
+          });
+        Swal.fire("Deleted!", "User has been deleted.", "success");
+      }
+    });
+  };
+
   return (
-    <div>UserList</div>
-  )
+    <div className="p-2 shadow-lg rounded-xl border">
+      <h1 className="text-center text-4xl font-semibold mb-0">User List</h1>
+      <div className="divider mt-0"></div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr className="text-lg bg-[#004bad2d] rounded-lg">
+              <th className="font-bold">ID</th>
+              <th className="font-bold">Name</th>
+              <th className="font-bold">Email</th>
+              <th className="font-bold">Status</th>
+              <th className="font-bold text-center">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentItems.map((user, index) => (
+              <tr
+                key={index}
+                className="hover bg-slate-100 border-b border-gray-300 text-lg"
+              >
+                <th>{user.id}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.status}</td>
+                <td width="30">
+                  <div className="flex gap-5">
+                    <button className="btn btn-sm bg-blue-200">Edit</button>
+                    <button
+                      onClick={() => handleDelete(user.id)}
+                      className="btn btn-sm bg-red-200"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-between mt-3">
+        <div className="flex items-center">
+          <button
+            className="btn btn-sm bg-blue-200 mr-2"
+            disabled={currentPage === 1}
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
+            Previous
+          </button>
+          {renderPageNumbers()}
+          <button
+            className="btn btn-sm bg-blue-200"
+            disabled={currentPage === totalPages}
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
+            Next
+          </button>
+        </div>
+        <div className="flex items-center">
+          <input
+            id="jumpPageInput"
+            type="number"
+            min={1}
+            max={totalPages}
+            className="px-2 py-1 border rounded-lg mr-2"
+          />
+          <button
+            className="btn btn-sm bg-blue-200"
+            onClick={handleGoButtonClick}
+          >
+            Go
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
