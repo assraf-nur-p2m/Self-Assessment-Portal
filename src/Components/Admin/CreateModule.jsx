c8674f2149f30e5bc7f24e55176de44b3a820c93
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2023-08-29 14:56:29 +0600

    creating create module

--- src/Components/Admin/CreateModule.jsx
+++ src/Components/Admin/CreateModule.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function CreateModule() {
   return (
     <div>
-        
+      <div className="p-2 shadow-lg rounded-xl h-[96vh]">
+        <div>
+          <div className="form-control p-4">
+            <label className="label mb-0">
+              <span className="label-text mb-0">
+                Write your Module name....{" "}
+              </span>
+            </label>
+            <input
+              name="question"
+              type="text"
+              placeholder="Module Name.."
+              className="input input-bordered w-12/12 mt-0 shadow-md text-lg"
+            />
+          </div>
+
+          <div>
+            
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
