44da5268ca73e50ad6a44a4c67c41bfe29380778
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2023-09-02 15:14:19 +0600

    creaqting documnet

--- src/Components/Admin/SetModuleMaterials.jsx
+++ src/Components/Admin/SetModuleMaterials.jsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from "react";
 export default function SetModuleMaterials() {
   const [category, setCategory] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [selectedOption, setSelectedOption] = useState("");
-  const [documents, setDocuments] = useState([]);
+  const [selectedButton, setSelectedButton] = useState(""); // Track the selected button
+  const [apiData, setApiData] = useState([]);
 
   useEffect(() => {
     fetch("http://192.168.1.29:8081/admin/category")
@@ -14,38 +14,32 @@ export default function SetModuleMaterials() {
       });
   }, []);
 
-  useEffect(() => {
-    if (selectedCategory && selectedOption) {
-      fetch(`http://192.168.1.29:8081/${selectedOption}/${selectedCategory}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setDocuments(data);
-        });
-    } else {
-      setDocuments([]);
-    }
-  }, [selectedCategory, selectedOption]);
-
   const handleCategoryChange = (event) => {
-    const selectedCat = event.target.value;
-    setSelectedCategory(selectedCat);
+    setSelectedCategory(event.target.value);
   };
 
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
+  const fetchData = (buttonType) => {
+    // Make an API request based on the selected category and button type
+    const apiUrl = `http://192.168.1.29:8081/${buttonType}/${selectedCategory}`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setApiData(data);
+        setSelectedButton(buttonType); // Update the selected button
+      });
   };
 
   return (
     <div>
       <div className="p-2 shadow-lg rounded-xl h-auto">
-        <div className="flex justify-between items-center content-center">
+        <div className="flex justify-between content-center items-center">
           <div className="p-3 flex items-center gap-4">
             <p className="text-xl">Select Category</p>
             <select
               className="form-select border px-12 py-2 text-xl rounded-lg shadow-md"
               name="quizCategory"
-              value={selectedCategory}
               onChange={handleCategoryChange}
+              value={selectedCategory}
             >
               <option value="">Select a category</option>
               {category.map((cat) => (
@@ -55,47 +49,39 @@ export default function SetModuleMaterials() {
               ))}
             </select>
           </div>
-          <div className="flex gap-4">
+          <div className="flex gap-5 text-lg px-2">
             <button
-              className={`btn text-lg ${
-                selectedOption === "documents"
-                  ? "btn-primary text-white"
-                  : "btn-info"
+              className={`btn btn-info ${
+                selectedButton === "documents" ? "active" : ""
               }`}
-              onClick={() => handleOptionClick("documents")}
+              onClick={() => fetchData("documents")}
             >
-              Doc
+              Document
             </button>
             <button
-              className={`btn text-lg ${
-                selectedOption === "Video"
-                  ? "btn-primary text-white"
-                  : "btn-info"
+              className={`btn btn-info ${
+                selectedButton === "videos" ? "active" : ""
               }`}
-              onClick={() => handleOptionClick("Video")}
+              onClick={() => fetchData("video")}
             >
               Video
             </button>
             <button
-              className={`btn text-lg ${
-                selectedOption === "Question"
-                  ? "btn-primary text-white"
-                  : "btn-info"
+              className={`btn btn-info ${
+                selectedButton === "questions" ? "active" : ""
               }`}
-              onClick={() => handleOptionClick("Question")}
+              onClick={() => fetchData("questions")}
             >
               Question
             </button>
           </div>
         </div>
-        <hr className="mt-1" />
-        <div className="mt-4">
-          <p className="text-xl">File Names:</p>
-          <ul>
-            {documents.map((doc) => (
-              <li key={doc.id}>{doc.fileName}</li>
-            ))}
-          </ul>
+        <hr className="mt-2" />
+
+        {/* Display the fetched data */}
+        <div>
+          <h2>Fetched Data:</h2>
+          <pre>{JSON.stringify(apiData, null, 2)}</pre>
         </div>
       </div>
     </div>
